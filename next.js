e87e753f434ff4f22a29ce825f311c783ae35920
Smooke09 <pedrolucas1014zz@gmail.com>
2023-07-26 22:40:05 -0300

    chore: new settigns and fix tabs

--- next.js
+++ next.js
@@ -1,55 +1,32 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
-    jest: true,
+    node: true,
   },
-  extends: [
-    'standard',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['standard', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'jsx-a11y',
-    '@typescript-eslint'
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
-    'prettier/prettier': ["error", {
-      'printWidth': 80,
-      'tabWidth': 2,
-      'singleQuote': true,
-      'trailingComma': 'all',
-      'arrowParens': 'always',
-      'semi': false,
-      'endOfLine': 'auto',
-    }],
-    'jsx-a11y/alt-text': [
-      'warn',
+    'prettier/prettier': [
+      'error',
       {
-        elements: ['img'],
-        img: ['Image'],
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        semi: false,
       },
     ],
-    'jsx-a11y/aria-props': 'warn',
-    'jsx-a11y/aria-proptypes': 'warn',
-    'jsx-a11y/aria-unsupported-elements': 'warn',
-    'jsx-a11y/role-has-required-aria-props': 'warn',
-    'jsx-a11y/role-supports-aria-props': 'warn',
+    'camelcase': 'off',
   },
   settings: {
-    react: {
-      version: 'detect',
-    },
     'import/parsers': {
       [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
     },
-  }
-}
+  },
+};
